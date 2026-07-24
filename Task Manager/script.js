195faf46f9628ef2c327dde2f8b5c694693e09e5
Jadeja Mitrajsinh <mitrajsinhj08@gmail.com>
2026-07-24 15:58:54 +0530

    Add files via upload

---
+++ Task Manager/script.js
@@ -0,0 +1,98 @@
+function openAddCardModal() {
+  const modal = new bootstrap.Modal(document.getElementById("addCardModal"));
+  modal.show();
+}
+
+function addCard() {
+  const name = document.getElementById("floatingInput").value;
+
+  const description = document.getElementById("floatingDescription").value;
+
+  const date = document.getElementById("floatingDate").value;
+
+  let cardContainer = document.getElementById("card-container");
+
+  cardContainer.innerHTML += `
+    <div class="col">   
+        <div class="card h-100">
+            <div class="card-body">
+                <h1 class="card-title">${name}</h1>
+                <h4 class="card-text">${date}</h4>
+                <h5 class="card-text">${description}</h5>
+            </div>
+            <div class="card-button">
+              <button class="btn btn-danger m-3" onClick="deleteCard(this)">Delete</button>
+              <button class="btn btn-primary m-3" onClick="editCard(this)">Edit</button>
+            </div>
+        </div>
+    </div>
+    `;
+  alert("Task added successfully!");
+}
+
+function deleteCard(button) {
+  const card = button.closest(".col");
+  card.remove();
+
+  alert("Task deleted successfully!");
+}
+
+function editCard(button)
+{
+  const modal = new bootstrap.Modal(document.getElementById("editModal"));
+  modal.show();
+
+  let name = document.getElementById("editfloatingInput");
+  let body = document.getElementById("editfloatingDescription");
+  let date = document.getElementById("editfloatingDate");
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
